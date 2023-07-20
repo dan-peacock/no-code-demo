f52e170e53dffcd6336cad8670b07f2d2e29b0e5
danpeacock <danpeacock96@gmail.com>
2023-07-21 00:05:10 +0100

    add ami logic

--- main.tf
+++ main.tf
@@ -2,10 +2,27 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Data source to get the latest Amazon Linux 2 AMI ID for the selected region
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["amazon"]
+}
+
 resource "aws_instance" "no-code" {
-    ami = "ami-0e1c5be2aa956338b"
-    instance_type = "t2.micro"
-    tags = {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  tags = {
     Name = var.vm_name
   }
 }
